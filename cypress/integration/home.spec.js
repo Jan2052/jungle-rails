b68b5e1211fc6cfcdd50d02f033db8c8f06530a2
Janet <Janet_Lam@live.ca>
2023-02-18 04:35:17 -0500

    test home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,14 @@
+describe('Visit home page', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:3000')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length.greaterThan", 1);
+  });
+
+});
